c6d81f349ed91d400581df66d69e99e35756a330
iis istiqomah <iisistiqomah20@gmail.com>
2023-07-26 17:10:25 +0700

    update

--- src/controllers/c_users.js
+++ src/controllers/c_users.js
@@ -42,7 +42,7 @@ const allData = async (req, res) => {
     try {
         const data = await ModelUser.find();
 
-        Messages(res, 200, "Get All Data User Success!", data);
+        Messages(res, 200, "Success!", data);
     } catch (error) {
         Messages(res, 500, error?.messages | "Internal Server Error");
     }
